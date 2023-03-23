3b9ff036213874fac5d9d28cec09ac3889ba3c64
Abhishek Swamy <swamy.aman@outlook.com>
2023-03-23 16:57:53 +1200

    Updated TestSuit.py

--- TestSuit.py
+++ TestSuit.py
@@ -6,8 +6,11 @@ if __name__ == '__main__':
     BASE_DIR = os.getcwd()
     # test script directory
     test_dir = os.path.join(BASE_DIR, 'cypress/e2e')
+    # browsers to run tests on
+    browsers = ["chrome", "edge"]
 
     # npx executable path
     npx_path = shutil.which('npx')
 
-    subprocess.run([f"{npx_path}", "cypress", "run", "--spec", f"{test_dir}"])
+    for browser in browsers:
+        subprocess.run([f"{npx_path}", "cypress", "run", "--browser", f"{browser}", "--spec", f"{test_dir}"])
